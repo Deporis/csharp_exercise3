b89fe137726dd4bab483ba8a68fd7f0c1ee21784
Alfred Inga <alfredingarios@gmail.com>
2020-08-29 01:20:46 +0200

    Fase 3

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace NomCiutats
         static void Fase12()
         {
             Console.WriteLine("Type the name of 6 cities:");
-            string city1 = Console.ReadLine().ToLower();
-            string city2 = Console.ReadLine().ToLower();
-            string city3 = Console.ReadLine().ToLower();
-            string city4 = Console.ReadLine().ToLower();
-            string city5 = Console.ReadLine().ToLower();
-            string city6 = Console.ReadLine().ToLower();
+            string city1 = Console.ReadLine();
+            string city2 = Console.ReadLine();
+            string city3 = Console.ReadLine();
+            string city4 = Console.ReadLine();
+            string city5 = Console.ReadLine();
+            string city6 = Console.ReadLine();
             Console.WriteLine($"{city1}, {city2}, {city3}, {city4}, {city5}, {city6}");
 
             string [] cities = new string[6] {city1, city2, city3, city4, city5, city6};
@@ -25,6 +25,12 @@ namespace NomCiutats
             int min = cities[0][0];
             string mincity = cities[0];
             int k = 0;
+
+            // Comparamos todas las posibilidades de nuestra array cities, calculando qué char
+            // es el más pequeño según el código ASCII. En el caso que la primera letra sean iguales
+            // comparamos la segunda letra, para poder editar el array cities y no repetir valores
+            // el último añadido se convierte en tilde, qué es el char más grande de ASCII imprimibles
+
             while(!(k == cities.Length))
             {
                 for(int i = 0; i < cities.Length; i++)
@@ -57,9 +63,37 @@ namespace NomCiutats
                 mincity = cities[0];
                 min = cities[0][0];
             }
+            // Checking if it is the correct order
             foreach(string city in sorted)
             {
                 Console.WriteLine(city);
+            }
+            
+            Fase3(sorted);
+        }
+        static void Fase3(string [] sortedArray)
+        {
+            string [] arrayCiutatsModificades = new string[sortedArray.Length];
+
+            for(int city = 0; city < sortedArray.Length; city++)
+            {
+                for(int letter = 0; letter < sortedArray[city].Length; letter++)
+                {
+                    string strLetter = sortedArray[city][letter].ToString();
+                    if(strLetter == "a")
+                    {
+                        strLetter = "4";
+                        arrayCiutatsModificades[city] += strLetter;
+                    }
+                    else
+                    {
+                        arrayCiutatsModificades[city] += strLetter;
+                    }
+                }
+            }
+            foreach(string i in arrayCiutatsModificades)
+            {
+                Console.WriteLine(i);
             }
         }
     }
